406523a59324c71dca0d96fc459dbdb9b3b81fcd
Jacob Stevenson <jasteve@microsoft.com>
2020-12-30 09:34:43 +0000

    add back in missing __ini__ changes

--- KustoPandas/expression_parser/__init__.py
+++ KustoPandas/expression_parser/__init__.py
@@ -1,3 +1,3 @@
 from .expression_parser import parse_expression
-from .expression_parser_types import Assignment, Method, Var, By, Comma
+from .expression_parser_types import Assignment, Method, Var, By, Comma, Star
 from .expression_tree import flatten_comma
