2c372754360fd4536f258f656287ea6d6bb2f36a
Jacob Stevenson <jasteve@microsoft.com>
2022-10-19 17:01:05 +0100

    update readme

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name='KustoPandas',
+setup(name='kusto_pandas',
       version='0.1.0',
       description='A wrapper around a Pandas DataFrame which allows you to use the syntax of the Kusto Query Language to transform the DataFrame',
       author='Jacob Stevenson',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['KustoPandas'],
+      url='https://github.com/js850/KustoPandas',
+      packages=['kusto_pandas'],
      )
