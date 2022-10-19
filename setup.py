56c24ea78f909fde57475ab880457ecd65931f9b
Jacob Stevenson <jasteve@microsoft.com>
2022-10-19 16:58:45 +0100

    add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='KustoPandas',
+      version='0.1.0',
+      description='A wrapper around a Pandas DataFrame which allows you to use the syntax of the Kusto Query Language to transform the DataFrame',
+      author='Jacob Stevenson',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['KustoPandas'],
+     )
