e9bb0ae4a08b59fc1dfe6f6d5fdae26550847a1d
Jacob Stevenson <jasteve@microsoft.com>
2022-10-21 14:49:16 +0100

    silence warning

--- kusto_pandas/expression_parser/expression_tree.py
+++ kusto_pandas/expression_parser/expression_tree.py
@@ -122,7 +122,7 @@ def convert_to_method_args(parsed):
     return Args([parsed])
 
 def find_matching_parentheses(line, left="(", right=")"):
-    matches = np.zeros(len(line), dtype=np.int)
+    matches = np.zeros(len(line), dtype=int)
     stack = []
     for i, c in enumerate(line):
         #print(c)
